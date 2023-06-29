36261e7a95b8109a40544b258a9c1844871e29ef
Fezzopro <fezzopro@gmail.com>
2023-06-29 11:13:04 +0200

    Add Reducers to the project

---
+++ src/redux/crypto/cryptoSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { API_URL } from '../../config/globals';
+
+const initialState = {
+  cryptos: [],
+  stats: {},
+  title: 'Crypto Trend',
+  isLoading: true,
+  error: '',
+  message: '',
+};
+
+export const fetchCrypto = createAsyncThunk(('crypto/fetchCrypto'), () => (
+  axios
+    .get(API_URL)
+    .then((response) => response.data.data)
+));
+
+export const cryptoSlice = createSlice({
+  name: 'crypto',
+  initialState,
+  reduces: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCrypto.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchCrypto.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cryptos = action.payload.coins;
+        state.stats = action.payload.stats;
+      })
+      .addCase(fetchCrypto.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default cryptoSlice.reducer;
