b7ca1f367737da0e9ccaf0e08c3efe88a24bd08b
Fezzopro <fezzopro@gmail.com>
2023-06-30 00:32:42 +0200

    Add Home page component and relevant CSS style rules to the project

---
+++ src/components/Home/Home.js
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import Stats from '../Stats/Stats';
+import Coin from '../Coin/Coin';
+import './home.css';
+
+export const CoinsList = ({ coins }) => {
+  const coinsList = coins.map((coin) => <Coin coin={coin} key={Math.random()} />);
+  return (
+    <ul className="crypto-list">
+      {coinsList}
+    </ul>
+  );
+};
+const Home = () => {
+  const {
+    cryptos, stats, isLoading, error,
+  } = useSelector((state) => state.crypto);
+
+  return (
+
+    <div className="content">
+      {error !== '' ? <div className="error"><span>{error}</span></div> : null}
+      {isLoading ? <div className="loading"><span /></div>
+        : (
+          <div>
+            <div className="all-stats">ALL STATS</div>
+            <Stats stats={stats} />
+            <CoinsList coins={cryptos} />
+          </div>
+        )}
+    </div>
+  );
+};
+
+CoinsList.propTypes = {
+  coins: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+};
+
+export default Home;
